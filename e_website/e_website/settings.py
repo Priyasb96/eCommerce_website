b5793025480864dbe5faca26a807de9ca51cedff
Priyasb96 <priyasb21@gmail.com>
2025-06-25 09:01:10 +0530

    application setting

--- e_website/e_website/settings.py
+++ e_website/e_website/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'product',  #custom app for app product management
 ]
 
 MIDDLEWARE = [
